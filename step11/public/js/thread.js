4b5153f8a43587a937e0fc1cb202b231131addb3
LiKe241 <43659250+LiKe241@users.noreply.github.com>
2019-09-24 11:13:43 -0700

    finished /thread/:id client side js code

--- step11/public/js/thread.js
+++ step11/public/js/thread.js
@@ -1,8 +1,7 @@
-
-
 /* eslint-disable no-undef */
-function postResponse(toReplace) {
-  const threadID = toReplace.className;
+// replaces toReplace with interface of posting response
+function displayResponseForm(toReplace) {
+  const threadID = $(toReplace).attr('class');
   const $newDiv = $('<div>', { class: threadID });
   const $form = $('<form>', { method: 'post', action: threadID})
     .appendTo($newDiv);
@@ -10,89 +9,77 @@ function postResponse(toReplace) {
   $('<button>', { text: 'post response' }).appendTo($form);
   $('<button>', {
     text: 'cancel response',
-    click() { restoreToButton($newDiv, 'response'); }
+    click: () => restoreToButton($newDiv, 'response')
   }).appendTo($newDiv);
   $(toReplace).replaceWith($newDiv);
 }
 
-function modify(toReplace) {
-  // reads from form and put to threadID/
+// replaces toReplace with interface of posting modification
+function displayModificationForm(toReplace) {
+  const threadID = $(toReplace).attr('class');
+  const $newDiv = $('<div>', { class: threadID });
+  const $content = $('<textarea>', {
+    name: 'content',
+    text: $('#p' + threadID).text()
+  }).appendTo($newDiv);
+  $('<button>', {
+    text: 'post modification',
+    click: () => postModification($newDiv, $content, threadID)
+  }).appendTo($newDiv);
+  $('<button>', {
+    text: 'cancel modification',
+    click: () => restoreToButton($newDiv, 'modification')
+  }).appendTo($newDiv);
+  $(toReplace).replaceWith($newDiv);
 }
 
-// replaces button to response element
-function replaceToInput(toReplace, type) {
-  const newDiv = document.createElement('DIV');
-  newDiv.setAttribute('id', 'd' + toReplace.className);
-  const form = document.createElement('FORM');
-  form.setAttribute('method', 'post');
-  form.setAttribute('action', '/' + type);
-  const content = document.createElement('TEXTAREA');
-  content.setAttribute('name', 'content');
-  if (type === 'modification') {
-    const originalContent = document.getElementById('p' + toReplace.className);
-    content.innerText = originalContent.innerText;
-  }
-  form.appendChild(content);
-  const submit = document.createElement('BUTTON');
-  submit.setAttribute('type', 'submit');
-  submit.innerText = 'post ' + type;
-  form.appendChild(submit);
-  const toUpdateID = document.createElement('INPUT');
-  toUpdateID.setAttribute('name', 'toUpdateID');
-  toUpdateID.setAttribute('value', toReplace.className);
-  toUpdateID.style.visibility = 'hidden';
-  form.appendChild(toUpdateID);
-  const threadID = document.createElement('INPUT');
-  threadID.setAttribute('name', 'threadID');
-  threadID.setAttribute('value', window.location.href.split('=')[1]);
-  threadID.style.visibility = 'hidden';
-  form.appendChild(threadID);
-  newDiv.appendChild(form);
-  const cancel = document.createElement('BUTTON');
-  cancel.setAttribute('type', 'button');
-  cancel.setAttribute(
-    'onclick',
-    'restoreToButton("' + toReplace.className + '", "' + type + '")'
-  );
-  cancel.innerText = 'cancel ' + type;
-  newDiv.appendChild(cancel);
-  toReplace.parentNode.replaceChild(newDiv, toReplace);
+// uploads content via HTTP PUT to server
+// replaces $toReplace with response text
+function postModification($toReplace, $content, threadID) {
+  const $postingText = $('<p>').text('posting modification...');
+  $.ajax({
+    url: threadID,
+    type: 'PUT',
+    data: 'content=' + $content.val(),
+    error: (res) => {
+      $postingText.replaceWith($('<p>').text(
+        'error ' + res.status + ': ' + res.statusText
+      ));
+    },
+    success: (res) => {
+      location.reload();
+    }
+  });
+  $toReplace.replaceWith($postingText);
 }
 
 // restores response elements back to button
 function restoreToButton(toReplace, type) {
-  // const newButton = document.createElement('BUTTON');
-  // newButton.setAttribute('class', toReplace.className);
-  // newButton.setAttribute('onclick', 'replaceToInput(this, "' + type + '")');
-  // if (type === 'response') {
-  //   newButton.innerText = 'post response';
-  // } else if (type === 'modification') {
-  //   newButton.innerText = 'modify';
-  // }
-  const $button = $('<button>', {
-    class: toReplace.className,
-    text: 'post response',
-    click() { postResponse($button); }
-  });
+  const $button = $('<button>', { class: toReplace.attr('class') });
+  if (type === 'response') {
+    $button.text('post response');
+    $button.click(() => displayResponseForm($button));
+  } else if (type === 'modification') {
+    $button.text('modify');
+    $button.click(() => displayModificationForm($button));
+  }
   $(toReplace).replaceWith($button);
 }
 
 // sends request to delete post
 function deleteThread(toDelete) {
-  const toDeleteID = toDelete.className;
-  const xhttp = new XMLHttpRequest();
-  const deletingText = document.createElement('P');
-  deletingText.innerText = 'deleting...';
-  xhttp.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        window.location.reload(false);
-      } else {
-        deletingText.innerText = 'deletion failed';
-      }
+  const $deletingText = $('<p>').text('deleting...');
+  $.ajax({
+    url: toDelete.className,
+    type: 'DELETE',
+    error: (res) => {
+      $deletingText.replaceWith($('<p>').text(
+        'error ' + res.status + ': ' + res.statusText
+      ));
+    },
+    success: (res) => {
+      location.reload();
     }
-  };
-  xhttp.open('DELETE', '/thread/' + toDelete.className, true);
-  xhttp.send();
-  toDelete.parentNode.replaceChild(deletingText, toDelete);
+  });
+  $(toDelete).replaceWith($deletingText);
 }
